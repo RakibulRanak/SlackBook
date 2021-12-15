d03a5c41fc85d10e3dadacca7b92e0bb092a9117
RakibulRanak <rakibulhasanranak1@gmail.com>
2021-12-15 13:13:29 +0600

    remove comments

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const axios = require('axios')
 require('dotenv').config();
 const { WebClient } = require('@slack/web-api');
 const { createEventAdapter } = require('@slack/events-api');
+const FB = require("fb");
 
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 const slackToken = process.env.SLACK_TOKEN;
 const port = process.env.SLACK_PORT || 3000;
-const fbUrl = process.env.FB_URL
-const FB = require("fb");
+
 const slackEvents = createEventAdapter(slackSigningSecret);
 const slackClient = new WebClient(slackToken);
 
@@ -16,16 +16,17 @@ FB.setAccessToken(process.env.FB_ACCESS_TOKEN);
 
 slackEvents.on('message', (event) => {
     console.log(`Got message from user ${event.user}: ${event.text}`);
-    const message = event.text;
+    var message = event.text;
     (async () => {
         try {
-            if (message.includes("#gunda")) {
-                console.log("ddddd")
-                FB.api('/950848322519530/feed', 'POST',{message}, function(
+            if (message.includes("#fbpost ")) {
+                console.log("Going to post in FB!")
+                message = message.replace("#fbpost ", "");
+                FB.api('/950848322519530/feed', 'POST', { message }, function (
                     response
-                  ) {
-                    console.log(response);
-                  });
+                ) {
+                    //console.log(response);
+                });
             }
             if (message === 'greet me') {
                 await slackClient.chat.postMessage({ channel: event.channel, text: `Hello <@${event.user}>! :tada:` })
