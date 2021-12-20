821e7b16af35f742208dea43c60f75a4b14f5472
RakibulRanak <rakibulhasanranak1@gmail.com>
2021-12-20 19:08:24 +0600

    deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { WebClient } = require('@slack/web-api');
 const { createEventAdapter } = require('@slack/events-api');
 const FB = require("fb");
-
+const myMap = require('./utils')
 
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 const slackToken = process.env.SLACK_TOKEN;
@@ -12,15 +12,28 @@ const groupUrl = process.env.GROUP_URL;
 
 const slackEvents = createEventAdapter(slackSigningSecret);
 const slackClient = new WebClient(slackToken);
-
+//console.log(`\udc79`)
 FB.setAccessToken(process.env.FB_ACCESS_TOKEN);
 
+const convertFormat = (str) => {
+    name = "";
+    for (i = 0; i < str.length; i++) {
+        let charCode = str.charCodeAt(i) - 97;
+        if (charCode >= 0) {
+            name += myMap.get(str.charAt(i))
+        }
+        else {
+            name += str.charAt(i)
+        }
+    }
+    return name;
+}
 
 slackEvents.on('message', async (event) => {
     console.log(`Got message from user <@${event.user}>: ${event.text}`);
     const userInfo = await slackClient.users.info({
         user: event.user
-      });
+    });
     const username = userInfo.user.name
     console.log(username)
     let message = event.text;
@@ -29,10 +42,10 @@ slackEvents.on('message', async (event) => {
             if (message.includes("#fbpost ")) {
                 console.log("Going to post in FB!")
                 message = message.replace("#fbpost ", "");
-                message += `\n Posted By ${username}`;
-                FB.api(groupUrl, 'POST', { message }, function (
-                    response
-                ) {
+                formatedUsername = convertFormat(username)
+                message += `\n ${formatedUsername}`;
+                // let aa = `\u5E73\u621015`;
+                FB.api(groupUrl, 'POST', { message }, function (response) {
                     console.log(response);
                 });
             }
---
+++ utils.js
@@ -0,0 +1,32 @@
+myformat = "𝖆𝖇𝖈𝖉𝖊𝖋𝖌𝖍𝖎𝖏𝖐𝖑𝖒𝖓𝖔𝖕𝖖𝖗𝖘𝖙𝖚𝖛𝖜𝖝𝖞𝖟"
+myMap = new Map()
+
+myMap.set("a", "𝖆");
+myMap.set("b", "𝖇");
+myMap.set("c", "𝖈");
+myMap.set("d", "𝖉");
+myMap.set("e", "𝖊");
+myMap.set("f", "𝖋");
+myMap.set("g", "𝖌");
+myMap.set("h", "𝖍");
+myMap.set("i", "𝖎");
+myMap.set("j", "𝖏");
+myMap.set("k", "𝖐");
+myMap.set("l", "𝖑");
+myMap.set("m", "𝖒");
+myMap.set("n", "𝖓");
+myMap.set("o", "𝖔");
+myMap.set("p", "𝖕");
+myMap.set("q", "𝖖");
+myMap.set("r", "𝖗");
+myMap.set("s", "𝖘");
+myMap.set("t", "𝖙");
+myMap.set("u", "𝖚");
+myMap.set("v", "𝖛");
+myMap.set("w", "𝖜");
+myMap.set("x", "𝖝");
+myMap.set("y", "𝖞");
+myMap.set("z", "𝖟");
+
+// export default { myMap }
+module.exports = myMap;
