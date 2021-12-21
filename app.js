8ad9713d663c2788b28a48be399eb19748eda98d
Durjoy001 <durjoydey001@gmail.com>
2021-12-21 13:23:08 +0600

    change user id to display name in fbpost

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const axios = require('axios')
-require('dotenv').config();
 const { WebClient } = require('@slack/web-api');
 const { createEventAdapter } = require('@slack/events-api');
 const FB = require("fb");
 const myMap = require('./utils')
-
+require('dotenv').config();
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 const slackToken = process.env.SLACK_TOKEN;
 const port = process.env.PORT || 3000;
 const groupUrl = process.env.GROUP_URL;
-
 const slackEvents = createEventAdapter(slackSigningSecret);
 const slackClient = new WebClient(slackToken);
-//console.log(`\udc79`)
+
 FB.setAccessToken(process.env.FB_ACCESS_TOKEN);
 
 const convertFormat = (str) => {
@@ -29,20 +27,28 @@ slackEvents.on('message', async (event) => {
         user: event.user
     });
     const username = userInfo.user.name
-    console.log(username)
     let message = event.text;
     (async () => {
         try {
             if (message.includes("#fbpost")) {
                 console.log("Going to post in FB!")
-                message = message.replace(" #fbpost ", "");
-                message = message.replace("#fbpost ", "");
-                message = message.replace(" #fbpost", "");
+                const regex = /<@[a-zA-Z0-9]{11}>/g;
+                const mentions = message.match(regex);
+                if(mentions){
+                    for(let i=0;i<mentions.length;i++){
+                        console.log(mentions[i].substring(2,13));
+                        const mentionedUserInfo = await slackClient.users.info({
+                            user: mentions[i].substring(2,13)
+                        });
+                        let userName = mentionedUserInfo.user.profile.display_name;
+                        if(!mentionedUserInfo.user.profile.display_name)
+                            userName = mentionedUserInfo.user.name;
+                        message = message.replace(mentions[i],'@'+ userName);
+                    }
+                }
                 message = message.replace("#fbpost", "");
-
                 formatedUsername = convertFormat(username)
                 message = formatedUsername + "@ˢˡᵃᶜᵏ" + `\n\n${message}`;
-                // let aa = `\u5E73\u621015`;
                 FB.api(groupUrl, 'POST', { message }, function (response) {
                     console.log(response);
                 });
@@ -51,14 +57,11 @@ slackEvents.on('message', async (event) => {
                 await slackClient.chat.postMessage({ channel: event.channel, text: `Hello <@${event.user}>! :tada:` })
             }
         } catch (error) {
-            console.log("vul hoyeche")
-            console.log(error)
+            console.log(error.data)
         }
     })();
 });
-
 slackEvents.on('error', console.error);
-
 slackEvents.start(port).then(() => {
     console.log(`Server started on port ${port}`)
 });
