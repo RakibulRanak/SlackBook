9c403fe98c0e8c5daec2d27cceea68d6e2f2c5b9
Jagonmoy <jagonmoydey1997@gmail.com>
2021-12-21 18:12:45 +0600

    add image functionality

---
+++ crawler.js
@@ -0,0 +1,19 @@
+const Crawler = require("crawler");
+
+exports.crawl =(urlsite)=>{
+    return new Promise((resolve,reject)=>{
+        var c = new Crawler({
+            callback : function (error, res, done) {
+                const image = res.$('img');
+                if(error){
+                    console.log(error);
+                    reject(error);
+                }else{
+                     url = image[0].attribs.src
+                     resolve(url);
+                }
+                done();
+            }
+        });
+        c.queue(urlsite);
+})};
