fb363f40ff935acf145d221674950c4175af7906
RakibulRanak <rakibulhasanranak1@gmail.com>
2021-12-22 17:14:14 +0600

    deploy updates

---
+++ slashCommand.js
@@ -0,0 +1,16 @@
+exports.serve = async (req, res, next) => {
+    try {
+        console.log(req.body)
+        if (req.body.token === slackVerificationToken && req.body.command === '/zork') {
+
+            axios.post(req.body.response_url, {
+                response_type: "in_channel",
+                text: "aaa",
+            })
+            return res.status(200).send()
+
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
