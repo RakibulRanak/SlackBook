d017879ffe54dd4cc88a8a3def9a708a1a68c0a6
RakibulRanak <rakibulhasanranak1@gmail.com>
2021-12-23 14:24:00 +0600

    deploy

--- controller/slackCommandController.js
+++ controller/slackCommandController.js
@@ -1,18 +1,38 @@
+'use strict'
 const slackVerificationToken = process.env.SLACK_VERIFICATION_TOKEN;
 const axios = require('axios')
-
+const Converter = require('timestamp-conv');
 exports.serve = async (req, res, next) => {
     try {
         console.log(req.body)
         if (req.body.token === slackVerificationToken) {
 
             if (req.body.command === '/weather') {
-                const weatherData = await axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=23.75280331031433&lon=90.37563165753778&exclude=hourly,daily&appid=d361ccad530aae28dce5d7a8e04b240d')
+                let weatherData = await axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=23.75280331031433&lon=90.37563165753778&exclude=hourly,daily,minutely&appid=d361ccad530aae28dce5d7a8e04b240d&units=metric')
+                // console.log(weatherData)
+
+                weatherData = weatherData.data.current;
                 console.log(weatherData)
+                const currentDate = new Converter.date(weatherData.dt, { forceTimezone: true, timezone: 6 });
+                const sunRiseDate = new Converter.date(weatherData.sunrise, { forceTimezone: true, timezone: 6 });
+                const sunSetDate = new Converter.date(weatherData.sunset, { forceTimezone: true, timezone: 6 });
+                const currentTime = `${currentDate.getHour()}:${currentDate.getMinute()}`
+                const sunRise = `${sunRiseDate.getHour()}:${sunRiseDate.getMinute()}`
+                const sunSet = `${sunSetDate.getHour()}:${sunSetDate.getMinute()}`
+
+                const message = `Current weather report near CEFALO at ${currentTime}. \n Sunrise : ${sunRise}
+                Sunset : ${sunSet} \n Temperature : ${weatherData.temp}°C \n Wind Speed : ${1.61 * weatherData.wind_speed}Km/h
+                Condition : ${weatherData.weather[0].description}
+                
+                
+                
+                `
                 axios.post(req.body.response_url, {
                     response_type: "in_channel",
-                    text: "aaa",
+                    text: message,
                 })
+
+
                 return res.status(200).send()
             }
             else
