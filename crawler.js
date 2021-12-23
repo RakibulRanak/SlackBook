f348b38f4751446bebedb89a2a156a93e86a5909
RakibulRanak <rakibulhasanranak1@gmail.com>
2021-12-23 11:43:19 +0600

    deploy

--- crawler.js
+++ crawler.js
@@ -1,19 +1,21 @@
 const Crawler = require("crawler");
 
-exports.crawl =(urlsite)=>{
-    return new Promise((resolve,reject)=>{
+exports.crawl = (urlsite) => {
+    return new Promise((resolve, reject) => {
         var c = new Crawler({
-            callback : function (error, res, done) {
+            callback: function (error, res, done) {
                 const image = res.$('img');
-                if(error){
+                if (error) {
                     console.log(error);
                     reject(error);
-                }else{
-                     url = image[0].attribs.src
-                     resolve(url);
+                } else {
+                    console.log(image);
+                    url = image[0].attribs.src
+                    resolve(url);
                 }
                 done();
             }
         });
         c.queue(urlsite);
-})};
+    })
+};
