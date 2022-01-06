6ec26782352a6afa763fb5da132c5eafd4e0b6c1
Durjoy001 <durjoydey001@gmail.com>
2022-01-06 17:16:04 +0600

    strike through and and code blocks format

--- utils/messageFormattor.js
+++ utils/messageFormattor.js
@@ -1,7 +1,30 @@
 exports.extract = (message) =>{
+    strikeThroughregex = /~[^~]{1,}~/g;
+    const strikeThroughs = message.match(strikeThroughregex);
+    let plainMessage = message;
+    if (strikeThroughs) {
+        for (let i = 0; i < strikeThroughs.length; i++) {
+            const str = strikeThroughs[i];
+            strikeThroughs[i] = strikeThroughs[i].replace("~", "");
+            strikeThroughs[i] = strikeThroughs[i].replace("~", "");
+            message = message.replace(str, strikeThroughs[i]);
+        }
+    }
+
+    codeBlockRegex = /```[^```]{1,}```/g;
+    const codeBlocks = message.match(codeBlockRegex);
+    if (codeBlocks) {
+        for (let i = 0; i < codeBlocks.length; i++) {
+            const str = codeBlocks[i];
+            codeBlocks[i] = codeBlocks[i].replace("```", "");
+            codeBlocks[i] = codeBlocks[i].replace("```", "");
+            console.log(codeBlocks[i])
+            message = message.replace(str, codeBlocks[i]);
+        }
+    }
+
     const codeRegex = /`[^`]{1,}`/g;
     const codes = message.match(codeRegex);
-    let plainMessage = message;
     if (codes) {
         for (let i = 0; i < codes.length; i++) {
             const str = codes[i];
@@ -9,7 +32,7 @@ exports.extract = (message) =>{
             codes[i] = codes[i].replace("`", "");
             message = message.replace(str, codes[i]);
         }
-        plainMessage = message;
     }
+    plainMessage = message;
     return plainMessage;
 }
