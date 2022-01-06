f6faa22d91b816924f13a4f51cf0c21bad1cd9fd
Jagonmoy <jagonmoydey1997@gmail.com>
2022-01-06 15:55:20 +0600

    add bold extractor

--- controller/slackEventController.js
+++ controller/slackEventController.js
@@ -5,6 +5,7 @@ const format = require('../utils/formatter')
 const crawler = require('../utils/crawler')
 const linkExtractor = require('../utils/linkExtractor');
 const mentionExtractor = require('../utils/mentionExtractor');
+const boldExtractor = require('../utils/boldExtractor');
 const fbAPI = require('../utils/fbAPICaller')
 
 let prevEventId;
@@ -37,7 +38,7 @@ slackEvents.on('message', async (event) => {
                     const { links, formatedMessage } = linkExtractor.extract(message);
                     message = formatedMessage;
                     message = await mentionExtractor.extract(message);
-                    
+                    message = await boldExtractor.extract(message);
                    
                     if (message.match("&gt")) for ( let i = 0 ; i < message.length ; i++) message = message.replace("&gt;", "");
                     
