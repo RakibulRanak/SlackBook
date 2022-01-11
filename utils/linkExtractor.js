1f9db37625da115fbcce3c8c27600253889b91e2
RakibulRanak <rakibulhasanranak1@gmail.com>
2022-01-11 17:26:44 +0600

    delete new line

--- utils/linkExtractor.js
+++ utils/linkExtractor.js
@@ -1,25 +1,23 @@
-exports.extract = (message) =>{
+exports.extract = (message) => {
     const regex = /<http.[^<]{1,500}>/g;
-    const linkRegex =  /.[^|]{1,500}/g;
+    const linkRegex = /.[^|]{1,500}/g;
     const links = message.match(regex);
     let formatedMessage = message;
     if (links) {
         for (let i = 0; i < links.length; i++) {
             const unformattedLink = links[i];
             let ind = message.indexOf(links[i]);
-            links[i] = links[i].replace(links[i],links[i].substring(1,links[i].length-1));
+            links[i] = links[i].replace(links[i], links[i].substring(1, links[i].length - 1));
             const linkText = links[i].match(linkRegex);
-            if(linkText[1]){
+            if (linkText[1]) {
                 linkText[1] = linkText[1].replace("|", "");
-                if(linkText[0] != linkText[1]){
-                    message = message.substring(0, ind) + linkText[1] +": " + message.substring(ind);
-                    linkText[0] = linkText[0] + "\n";
-                }
+                if (linkText[0] != linkText[1])
+                    message = message.substring(0, ind) + linkText[1] + ": " + message.substring(ind);
                 links[i] = linkText[0];
             }
             message = message.replace(unformattedLink, links[i]);
         }
         formatedMessage = message;
     }
-    return {links,formatedMessage};
+    return { links, formatedMessage };
 }
