369bb30b1f8cd8617881daae7fd8ccb9ac87fcf6
Durjoy001 <durjoydey001@gmail.com>
2022-01-11 15:38:10 +0600

    format link text contained message

--- utils/linkExtractor.js
+++ utils/linkExtractor.js
@@ -7,14 +7,13 @@ exports.extract = (message) =>{
         for (let i = 0; i < links.length; i++) {
             const unformattedLink = links[i];
             let ind = message.indexOf(links[i]);
-            links[i] = links[i].replace("<", "");
-            links[i] = links[i].replace(">", "");
+            links[i] = links[i].replace(links[i],links[i].substring(1,links[i].length-1));
             const linkText = links[i].match(linkRegex);
             if(linkText[1]){
                 linkText[1] = linkText[1].replace("|", "");
                 if(linkText[0] != linkText[1]){
-                    const tmpMessage = message.substring(0, ind)  + linkText[1] +": " +message.substring(ind);
-                    message = tmpMessage;
+                    message = message.substring(0, ind) + linkText[1] +": " + message.substring(ind);
+                    linkText[0] = linkText[0] + "\n";
                 }
                 links[i] = linkText[0];
             }
