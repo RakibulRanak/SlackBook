95c54d31495c1e29f9548685b2eaf57f0bedd121
RakibulRanak <rakibulhasanranak1@gmail.com>
2022-02-02 13:06:53 +0600

    notify user

--- fb/fbTokenExpirationNotifier.js
+++ fb/fbTokenExpirationNotifier.js
@@ -5,7 +5,7 @@ const administrator_1 = process.env.ADMINISTRATOR_SLACK_MEMBER_ID_1;
 const administrator_2 = process.env.ADMINISTRATOR_SLACK_MEMBER_ID_2;
 
 exports.notify = async (event) => {
-    await slackClient.chat.postEphemeral({ thread_broadcast: false, thread_ts: event.thread_ts, channel: event.channel, user: event.user, text: '' })
+    await slackClient.chat.postEphemeral({ thread_broadcast: false, thread_ts: event.thread_ts, channel: event.channel, user: event.user, text: 'Your message was not posted on FB due to fb token expiration. A notification is also sent to your system administrator to refresh the token. Meanwhile sit back and try later!' })
     await slackClient.chat.postMessage({ channel: administrator_1, token: slackBotToken, text: `Facebook user access token is expired . \n Please sign in to facebook with your organizational facebook account . \n Then go to this link with the same browser https://developers.facebook.com/tools/explorer/ to generate new access token. \n and update your server environment` });
     await slackClient.chat.postMessage({ channel: administrator_2, token: slackBotToken, text: `Facebook user access token is expired . \n Please sign in to facebook with your organizational facebook account . \n Then go to this link with the same browser https://developers.facebook.com/tools/explorer/ to generate new access token. \n and update your server environment` });
 }
